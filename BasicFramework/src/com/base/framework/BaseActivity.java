6fa2dea0191f95e220565f994f7bcc229d88b543
lubyqing <363309734@qq.com>
2015-05-08 15:09:59 +0800

    empty project

---
+++ BasicFramework/src/com/base/framework/BaseActivity.java
@@ -0,0 +1,15 @@
+package com.base.framework;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BaseActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
